1b0289921c826c63c9b4b76469e380ad9ad585e8
Gary Daigle <gary.daigle@mister-e-liquid.com>
2015-05-25 18:08:34 -0400

    Added bins display, still needs totals

--- js/page_bins.js
+++ js/page_bins.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
     $.display.page.bins = {};
 
+    var binTemplate = $('#binTemplate').clone();
+    $('#binTemplate').remove();
+
     $.display.page.bins.load = function() {
         $.display.toggleLoadingScreen($.etsyDB.getAllBins($.display.page.bins.drawBins));
     };
 
     $.display.page.bins.drawBins = function(bins) {
-        console.log('Got bins');
-        console.log(bins);
+        var appendString = '';
+
+        $.each(bins, function(key, val) {
+            var bin = binTemplate;
+            bin.removeClass('template');
+
+            bin.find('.bin_id').html(val.id);
+            bin.find('.bin-name').html(val.name);
+            bin.find('.bin-notes').html(val.notes);
+
+            appendString += ('<tr>' + bin.html() + '</tr>');
+        });
+
+        // Try and cut some repaint time by using vanilla JS
+        var binsTableBody = document.getElementById('binsTableBody');
+        binsTableBody.innerHTML = appendString;
     };
 
     $('#createBin').on('click', function() {
