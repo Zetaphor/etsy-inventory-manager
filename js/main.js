ed1840f3b341f332c1899e0bcd3126f356d6dfd4
Gary Daigle <gary.daigle@mister-e-liquid.com>
2015-05-25 02:58:00 -0400

    Added update totals helper function

--- js/main.js
+++ js/main.js
@@ -3,16 +3,42 @@ $(document).ready(function() {
     $.db = new Dexie('etsyDB');
     $.etsyDB.init();
 
+    $.inventory = {
+        newProducts: 0,
+        totalBins: 0,
+        totalProducts: 0
+    };
+
     $.db.on('ready', function () {
         //$.etsyDB.deleteDatabase();
         //$.etsyDB.listTables();
 
         //$.etsy.getListings($.etsy.updateRecentListings, 0, $.etsy.recentResultsLimit);
-        $.etsy.getListings($.etsy.updateAllListings);
+        //$.etsy.getListings($.etsy.updateAllListings);
+
+        updateDisplayCounts();
 
         //$.etsyDB.exportDatabase().then(function (dbObj) {
         //    var json = JSON.stringify(dbObj);
         //    alert (json);
         //});
+
     });
+
+    function updateDisplayCounts() {
+        $.db.table('products').where('bin_id').equals(-1).count(function(count) {
+            $.inventory.newProducts = count;
+            $('.num-total-new').html(count);
+        });
+
+        $.db.table('bins').count(function(count) {
+            $.inventory.totalBins = count;
+            $('.num-total-bins').html(count);
+        });
+
+        $.db.table('products').count(function(count) {
+            $.inventory.totalProducts = count;
+            $('.num-total-products').html(count);
+        });
+    }
 });
