09cfc5d41f189632126b055e2606e21befb6be3b
Gary Daigle <gary.daigle@mister-e-liquid.com>
2015-05-25 16:07:29 -0400

    Added start of bins JS

--- js/page_bins.js
+++ js/page_bins.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $.display.page.bins = {};
 
     $.display.page.bins.load = function() {
-        console.log('Loaded bins');
+        $.display.toggleLoadingScreen();
     };
 });
