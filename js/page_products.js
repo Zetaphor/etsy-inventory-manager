9d101ee1e1e391d10acc2f1c4103c75d31a3e06c
Gary Daigle <gary.daigle@mister-e-liquid.com>
2015-05-25 05:13:09 -0400

    Added display content for all products

--- js/page_products.js
+++ js/page_products.js
@@ -2,6 +2,22 @@ $(document).ready(function() {
     $.display.page.products = {};
 
     $.display.page.products.load = function() {
-        console.log('Loaded products');
+        $.etsy.getAllProducts($.display.page.products.drawProducts);
+    };
+
+    $.display.page.products.drawProducts = function(products) {
+        $.each(products, function(key, val) {
+            var product = $('#productTemplate').clone();
+            product.removeClass('template');
+
+            product.find('.listing-id').html(val.listing_id);
+            product.find('.title').html(val.title);
+            product.find('.bin-id').html(val.bin_id);
+            product.find('.price').html(val.price);
+            product.find('.created').html(val.created);
+            product.find('.original-created').html(val.original_creation);
+
+            product.appendTo('#productsTable tbody');
+        });
     };
 });
