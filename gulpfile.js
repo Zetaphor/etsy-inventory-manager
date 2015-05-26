542a66d0b708c7bac95f4cdc9659cdb20a9b7a94
Gary Daigle <gary.daigle@mister-e-liquid.com>
2015-05-26 18:19:06 -0400

    Removed unused livereload

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,7 @@ var jshint = require('gulp-jshint'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
     fileinclude = require('gulp-file-include'),
-    concat = require('gulp-concat'),
-    livereload = require('gulp-livereload');
+    concat = require('gulp-concat')
 
 // Compile Sass
 gulp.task('sass', function() {
@@ -26,14 +25,7 @@ gulp.task('sass', function() {
         .pipe(concat('main.css'))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./css'))
-        //.pipe(livereload());
 });
-
-// Livereload HTML files
-//gulp.task('html', function() {
-//    gulp.src(['**/*.html', '!lib/**/*', '!node_modules/**/*'])
-//        .pipe(livereload());
-//});
 
 gulp.task('fileinclude', function() {
     gulp.src(['index.html'])
@@ -62,13 +54,10 @@ gulp.task('scripts', function() {
         .pipe(rename({extname: '.min.js'}))
         .pipe(sourcemaps.write('./_sourcemaps'))
         .pipe(gulp.dest('js/min'))
-        //.pipe(livereload());
 });
 
 // Watch files for changes
 gulp.task('watch', function() {
-    //livereload.listen();
-    //gulp.watch('**/*.html', ['html']);
     gulp.watch('**/*.html', ['fileinclude']);
     gulp.watch('js/**/*.js', ['scripts']);
     gulp.watch('scss/**/*.scss', ['sass']);
