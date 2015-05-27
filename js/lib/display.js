d137dadf0bea42a229e3f79bc0817ed29bfb2ca1
Gary Daigle <gary.daigle@mister-e-liquid.com>
2015-05-27 16:43:23 -0400

    Updated reference to inventory counts

--- js/lib/display.js
+++ js/lib/display.js
@@ -24,17 +24,17 @@ $(document).ready(function() {
 
         updateCounts: function() {
             $.db.table('products').where('bin_id').equals(-1).count(function(count) {
-                $.inventory.newProducts = count;
+                $.etsyApp.inventoryCounts.newProducts = count;
                 $('.num-total-new').html(count);
             });
 
             $.db.table('bins').count(function(count) {
-                $.inventory.totalBins = count;
+                $.etsyApp.inventoryCounts.totalBins = count;
                 $('.num-total-bins').html(count);
             });
 
             $.db.table('products').count(function(count) {
-                $.inventory.totalProducts = count;
+                $.etsyApp.inventoryCounts.totalProducts = count;
                 $('.num-total-products').html(count);
             });
         },
