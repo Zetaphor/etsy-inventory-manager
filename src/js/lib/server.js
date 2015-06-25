10ded7fe237c4f05fba45ec095819cb658750ca6
Gary Daigle <zetaphor@gmail.com>
2015-06-24 23:51:06 -0400

    Added catch for server not running

--- src/js/lib/server.js
+++ src/js/lib/server.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
     var socket = null;
     if ( typeof io === 'function' ) {
         socket = io.connect('http://localhost:8001');
-        if (socket) {
-            socket.on('connect', function() {
-                console.log('socket.io connected');
-            });
-        } else {
+        if (!socket) {
             console.log('io was available, but deskSock isn\'t available. Is the nwk-socket.io app running on your local machine and providing "somenamespace"?');
         }
     } else {
         console.log('io isn\'t available. Is the nwk-socket.io app running on your local machine?');
     }
+
+    socket.on('connect', function() {
+        console.log('socket.io connected');
+    });
+
+    socket.io.on('connect_error', function(err) {
+        // handle server error here
+        console.log('Error connecting to server');
+    });
 });
